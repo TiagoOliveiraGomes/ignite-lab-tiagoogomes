6052bae2a579019ff122f822a42ba28f4568b05c
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-06-25 22:33:14 -0300

    fix img name

--- src/pages/subscribe/index.tsx
+++ src/pages/subscribe/index.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { Logo } from '../../components/logo'
 import { useCreateSubscriberMutation } from '../../graphql/generated'
 import './styles.css'
-import backgroundImg from '../../assets/code-mockup.png'
+import backgroundImg from '../../assets/code-Mockup.png'
 
 export function Subscribe() {
     const navigate = useNavigate()
