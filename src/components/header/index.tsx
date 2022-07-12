4d17fb1aaaa08214a542f42ff3a8172ad136e70d
TiagoOliveiraGomes <tyagooGomes@gmail.com>
2022-07-12 14:01:12 -0300

    fix mobile menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,8 +4,10 @@ import { MenuItem } from '../menuItem'
 import './styles.css'
 
 interface HeaderProps {
-  setIsSidebarVisible: React.Dispatch<React.SetStateAction<boolean>>
-  isSidebarVisible: boolean
+  setIsSidebarVisible: React.Dispatch<React.SetStateAction<boolean>>,
+  isSidebarVisible: boolean,
+  setIsVideoVisible: React.Dispatch<React.SetStateAction<boolean>>,
+  isVideoVisible: boolean,
 }
 
 export default function Header(props:HeaderProps) {
@@ -14,7 +16,7 @@ export default function Header(props:HeaderProps) {
         <Logo />
         <div className='ml-5 flex gap-3 text-sm'>
           <h1>Aulas</h1>
-          <MenuItem isSidebarVisible={props.isSidebarVisible} setIsSidebarVisible={props.setIsSidebarVisible} />
+          <MenuItem isVideoVisible={props.isVideoVisible} isSidebarVisible={props.isSidebarVisible} setIsSidebarVisible={props.setIsSidebarVisible} setIsVideoVisible={props.setIsVideoVisible} />
         </div>
     </header>
   )
